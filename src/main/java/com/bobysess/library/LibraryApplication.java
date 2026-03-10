f825d390092ceea72ce653e78e031bb048987c6c
Arsel Tseumeugne <afeutse@contargo.net>
2026-03-10 06:05:43 +0100

    chore: init  spring boot app

---
+++ src/main/java/com/bobysess/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.bobysess.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
