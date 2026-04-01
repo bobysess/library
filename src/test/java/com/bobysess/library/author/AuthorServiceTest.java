360dc0b37b44c890d600783fbbc506f52aba2c1c
Arsel Tseumeugne <afeutse@contargo.net>
2026-04-01 14:41:01 +0200

    fix sonar issues

--- src/test/java/com/bobysess/library/author/AuthorServiceTest.java
+++ src/test/java/com/bobysess/library/author/AuthorServiceTest.java
@@ -3,7 +3,6 @@ package com.bobysess.library.author;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyIterable;
 import static org.mockito.Mockito.verify;
